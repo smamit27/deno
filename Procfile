8b07023f4e8ae3db2c1017d522631e1cb968ee63
Apple <apple@Apples-MacBook-Pro.local>
2020-05-31 11:27:42 +0530

    Data

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: deno run --allow-net=:${PORT} --allow-write --allow-read --allow-plugin --allow-env --unstable server.ts --port=${PORT}
+web: deno run --allow-net=:8080 --cached-only --allow-write --allow-read --allow-plugin --allow-env --unstable server.ts --port=8080
--- server.ts
+++ server.ts
@@ -2,11 +2,11 @@
 import {Application} from 'https://deno.land/x/oak/mod.ts';
 import router from './Router/router.ts';
 import { oakCors } from "https://deno.land/x/cors/mod.ts";
-import * as flags from "https://deno.land/std/flags/mod.ts";
+// import * as flags from "https://deno.land/std/flags/mod.ts";
 const {args,exit} = Deno;
 const DEFALT_PORT = 8080;
-const argsPort = flags.parse(args).port;
-const port = argsPort ? Number(argsPort) :DEFALT_PORT;
+// const argsPort = flags.parse(args).port;
+// const port = argsPort ? Number(argsPort) :DEFALT_PORT;
 let app = new Application();
 app.use(router.routes())
 app.use(
@@ -17,7 +17,7 @@ app.use(
     }),
   );
 app.use(router.allowedMethods())
-const options = { hostname: '0.0.0.0', port: 8000 };
+const options = { hostname: '0.0.0.0', port: 8080 };
 
 await app.listen(options);
   
