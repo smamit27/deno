de04ad0868c164c67ac89ff2be9c9b5d6678deee
Apple <apple@Apples-MacBook-Pro.local>
2020-05-31 11:30:37 +0530

    Dev

--- server.ts
+++ server.ts
@@ -2,11 +2,7 @@
 import {Application} from 'https://deno.land/x/oak/mod.ts';
 import router from './Router/router.ts';
 import { oakCors } from "https://deno.land/x/cors/mod.ts";
-// import * as flags from "https://deno.land/std/flags/mod.ts";
-const {args,exit} = Deno;
-const DEFALT_PORT = 8080;
-// const argsPort = flags.parse(args).port;
-// const port = argsPort ? Number(argsPort) :DEFALT_PORT;
+
 let app = new Application();
 app.use(router.routes())
 app.use(
